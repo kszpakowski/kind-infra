b0a661a3ba55986ff0973d7e8af870892de513c0
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-10-25 19:17:09 +0200

    rename argo infra into app of apps

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ kind create cluster --config cluster.yaml
 # Argo CD
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.1.3/manifests/install.yaml
-kubectl apply -f argo-infra-app.yaml
+kubectl apply -f app-of-apps.yaml
